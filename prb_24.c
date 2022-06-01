35aa20d18132960adf77412d434e12fc9a4a59b0
Atik Hasan <atikhasan801@gmail.com>
2022-06-01 21:50:21 +0600

    ceaser cipher enryption and decryption solved

---
+++ prb_24.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+void ceaser_cipher_encryption(char *, int);
+void ceaser_cipher_decryption(char *, int);
+
+int main()
+{
+
+    char message[200];
+    int key;
+    gets(message);
+    scanf("%d", &key);
+    ceaser_cipher_encryption(message, key);
+    puts(message);
+    ceaser_cipher_decryption(message, key);
+    puts(message);
+
+    return 0;
+}
+
+void ceaser_cipher_encryption(char *s, int key)
+{
+
+    int i = 0;
+    char ch;
+    while (s[i] != '\0')
+    {
+
+        if (s[i] >= 'a' && s[i] <= 'z')
+        {
+
+            ch = s[i] + key;
+            if (ch > 'z')
+            {
+                ch = ch - 'z' + 'a' - 1;
+            }
+            s[i] = ch;
+        }
+        else if (s[i] >= 'A' && s[i] <= 'Z')
+        {
+            ch = s[i] + key;
+
+            if (ch > 'Z')
+            {
+                ch = ch - 'Z' + 'A' - 1;
+            }
+            s[i] = ch;
+        }
+
+        i++;
+    }
+}
+
+void ceaser_cipher_decryption(char *s, int key)
+{
+
+    int i = 0;
+    char ch;
+    while (s[i] != '\0')
+    {
+
+        if (s[i] >= 'a' && s[i] <= 'z')
+        {
+
+            ch = s[i] - key;
+            if (ch < 'a')
+            {
+
+                ch = ch + 'z' - 'a' + 1;
+            }
+            s[i] = ch;
+        }
+        else if (s[i] >= 'A' && s[i] <= 'Z')
+        {
+            ch = s[i] - key;
+
+            if (ch < 'A')
+            {
+                ch = ch + 'Z' - 'A' + 1;
+            }
+            s[i] = ch;
+        }
+
+        i++;
+    }
+}
